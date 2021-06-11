7faafc8ce067c43a845680c49e093e0d7dca4236
saksshham <81289215+saksshham@users.noreply.github.com>
2021-06-12 05:01:52 +0530

    Added functions to the calculator

---
+++ DAY 5/work.js
@@ -0,0 +1,31 @@
+let screen = document.getElementById('input');
+buttons = document.querySelectorAll('button');
+let inputValue = '';
+
+for(item of buttons){
+    item.addEventListener('click', (e) => {
+        buttonText = e.target.innerText;
+        console.log('Button text is ', buttonText);
+
+         if(buttonText == '<'){
+            x =  inputValue.substr(0,inputValue.length-1);
+            inputValue = x;
+            screen.value = inputValue;
+        } else
+        if(buttonText == 'AC'){
+            inputValue = '';
+            screen.value = inputValue;
+        }
+        else if(buttonText == '='){
+            screen.value = eval(inputValue);
+        }
+        else{              
+            inputValue += buttonText;
+            screen.value = inputValue;
+        }
+
+    })
+}
+// function erase(){
+
+// }
