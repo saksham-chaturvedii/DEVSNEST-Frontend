b915334e6e43c5e6826bf79eb5a33ff0ea88a984
saksshham <81289215+saksshham@users.noreply.github.com>
2021-07-05 02:09:46 +0530

    Adding image file

---
+++ DAY 16/meme-card/src/components/card.jsx
@@ -0,0 +1,10 @@
+import bg from "./image.jsx";
+function Card(){
+    return(
+        <div>
+            <bg />
+            ALBUM NAME: ICARUS FALLS
+        </div>   
+    );
+}
+export default Card;
---
+++ DAY 16/meme-card/src/components/image.jsx
@@ -0,0 +1,8 @@
+import CardImg from "./CardImg.jpg";
+const bg = ()=>{
+    return (
+        
+        <img src={CardImg} alt="" />
+    );
+}
+export default bg;
