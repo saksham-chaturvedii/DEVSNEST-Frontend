1e4c6124d1411d896bb31a02b5a0e2a0ef4e7544
saksshham <81289215+saksshham@users.noreply.github.com>
2021-07-14 04:48:19 +0530

    Day 16 Prop part left

---
+++ react-js/src/App_17.jsx
@@ -0,0 +1,14 @@
+import Task from "./DAY 17/Task"
+import CalorieList from "./DAY 17/CalorieList"
+
+const build= ()=>{
+    return(
+        <div>
+            <Task />        
+            <CalorieList />
+        </div>
+
+    );
+
+}
+export default build;
---
+++ react-js/src/DAY 17/CalorieList.jsx
@@ -0,0 +1,16 @@
+
+const CalorieList = ()=>{
+    return(
+        <div>
+            <h2>CALORIE LIST</h2>
+            {/* <div className="container"> */}
+                <div className="list">
+                    {/* food */}
+                    {/* Consumption: x cals  */}
+                </div>
+            {/* </div> */}
+        </div>
+    )
+}
+
+export default CalorieList;
