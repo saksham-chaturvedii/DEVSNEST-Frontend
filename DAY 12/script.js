a9acf5ffa88bfcec3ed2f3935942727de0a64d58
saksshham <81289215+saksshham@users.noreply.github.com>
2021-07-17 17:34:55 +0530

    Day 18

---
+++ DAY 12/script.js
@@ -0,0 +1,52 @@
+const input = document.querySelector("input");
+const AddButton = document.querySelector("button");
+const todolist = document.querySelector(".todolist");
+
+/* Better Way to load the to-do entries*/
+var entries = [];
+
+AddButton.addEventListener("click", () => {
+  if (input.value.trim() === "") alert("Invalid Input");
+  else addEntries();
+});
+
+function addEntries() {
+  entries.push({ title: input.value.trim(), done: "false" });
+  const newEntry = document.createElement("div");
+  newEntry.innerHTML = entries[entries.length - 1].title;
+  todolist.appendChild(newEntry);
+  input.value = "";
+  localStorage.setItem("todo", JSON.stringify(entries));
+}
+
+window.onload = () => {
+  let localStorageData = localStorage.getItem("todo"); 
+  if (!localStorageData) return;
+
+  JSON.parse(localStorageData).forEach(function (item) {
+    entries.push(item);
+    const newEntry = document.createElement("div");
+    newEntry.innerHTML = item.title;
+    todolist.appendChild(newEntry);
+  });
+};
+// window.onload = () => {
+//   if (entries) {
+//     var entriez = localStorage.getItem("todo");
+//     entriez.push(localStorage.getItem("todo"));
+//     JSON.parse(entriez).forEach(function (item) {
+//       const newEntry = document.createElement("div");
+//       newEntry.innerHTML = item.title;
+//       todolist.appendChild(newEntry);
+//     });
+//   }
+// };
+
+/* Naive Way
+button.addEventListener('click', ()=>{
+    const entry = document.createElement('div');
+    entry.innerHTML = input.value;
+    todolist.appendChild(entry);
+    input.value="";
+})
+*/
