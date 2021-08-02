83a0fc6916ac84fe2fe6846d1f4ccc95c16427f8
saksshham <81289215+saksshham@users.noreply.github.com>
2021-08-02 23:53:54 +0530

    delete button mods

--- DAY 12/script.js
+++ DAY 12/script.js
@@ -1,8 +1,7 @@
 const input = document.querySelector("input");
-const AddButton = document.querySelector("button");
+const AddButton = document.getElementById("add_button");
 const todolist = document.querySelector(".todolist");
-const done = document.querySelector(".done")
-
+const done = document.querySelector(".done");
 /* Better Way to load the to-do entries*/
 var entries = [];
 
@@ -13,27 +12,45 @@ AddButton.addEventListener("click", () => {
 
 function addEntries() {
   entries.push({ title: input.value.trim(), done: "false" });
+  localStorage.setItem("todo", JSON.stringify(entries));
   const newEntry = document.createElement("div");
   newEntry.className = "entry";
-  newEntry.innerHTML = '→ ' + entries[entries.length - 1].title+
-    '<button class="done_button">DONE</button>';
+  newEntry.innerHTML = "→ " + entries[entries.length - 1].title;
+  const clear_button = document.createElement("button");
+  clear_button.setAttribute("class", "clear_button");
+  clear_button.innerText = "DONE";
+  newEntry.appendChild(clear_button);
   todolist.appendChild(newEntry);
   input.value = "";
-  localStorage.setItem("todo", JSON.stringify(entries));
+
+  clear_button.addEventListener("click", (e) => {
+    if (e.target.parentNode.tagName === "DIV") {
+      for (let i of entries) {
+        var todo_element_text = "→ " + i.title + "DONE";
+        if (todo_element_text === e.target.parentNode.textContent) {
+          entries.splice(todo_element_text, 1);
+          localStorage.setItem("todo", JSON.stringify(entries));
+        }
+      }
+      e.target.parentNode.remove();
+    }
+  });
 }
 
 window.onload = () => {
-  let localStorageData = localStorage.getItem("todo"); 
+  let localStorageData = localStorage.getItem("todo");
   if (!localStorageData) return;
 
   JSON.parse(localStorageData).forEach(function (item) {
+    console.log(item);
     entries.push(item);
     const newEntry = document.createElement("div");
     newEntry.className = "entry";
-    newEntry.innerHTML =
-      "→ " +
-      entries[entries.length - 1].title +
-      '<button class="done_button">DONE</button>';
+    newEntry.innerHTML = "→ " + entries[entries.length - 1].title;
+    var clear_button = document.createElement("button");
+    clear_button.setAttribute("class", "clear_button");
+    clear_button.innerText = "DONE";
+    newEntry.appendChild(clear_button);
     todolist.appendChild(newEntry);
   });
 };
@@ -41,7 +58,6 @@ window.onload = () => {
 // done.addEventListener('click',(e)=>{
 //   entries[e].pop
 // })
-
 
 /* Naive Way
 button.addEventListener('click', ()=>{
