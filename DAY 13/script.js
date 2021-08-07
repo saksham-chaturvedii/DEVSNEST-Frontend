d909d368c89d80c91a9bdbc673466ae7520e410a
saksshham <81289215+saksshham@users.noreply.github.com>
2021-08-07 23:01:47 +0530

    Appearance changes

--- DAY 13/script.js
+++ DAY 13/script.js
@@ -1,11 +1,11 @@
 const loadMeme = document.querySelector("button");
 const meme = document.querySelector(".meme");
 
-loadMeme.addEventListener('click', async ()=>{
-fetch('https://meme-api.herokuapp.com/gimme')
-.then(response => response.json())
-.then(data =>  {
-  console.log(data)
-  meme.src = data.url
-})
-})
+loadMeme.addEventListener("click", () => {
+  fetch("https://meme-api.herokuapp.com/gimme")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      meme.src = data.url;
+    });
+});
