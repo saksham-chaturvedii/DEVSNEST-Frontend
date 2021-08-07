f4cf40a8cd50bab55d0ff9a41c0225942e94f218
saksshham <81289215+saksshham@users.noreply.github.com>
2021-08-07 23:20:37 +0530

    Added Asyn Await

--- DAY 13/script.js
+++ DAY 13/script.js
@@ -1,11 +1,21 @@
 const loadMeme = document.querySelector("button");
 const meme = document.querySelector(".meme");
 
+/* USING "then" method
+
 loadMeme.addEventListener("click", () => {
-  fetch("https://meme-api.herokuapp.com/gimme")
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      meme.src = data.url;
-    });
+   fetch("https://meme-api.herokuapp.com/gimme")
+     .then((response) => response.json())
+     .then((data) => {
+       console.log(data);
+       meme.src = data.url;
+     });
+ });*/
+
+// USING "async-await" KEYWORDS
+
+loadMeme.addEventListener("click", async () => {
+  const response = await fetch("https://meme-api.herokuapp.com/gimme");
+  const memeJSON = await response.json();
+  meme.src = memeJSON.url;
 });
