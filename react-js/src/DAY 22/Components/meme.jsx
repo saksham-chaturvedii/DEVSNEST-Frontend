debe96b8069ac3d2ce2ba0748e44a05db7229a4e
saksshham <81289215+saksshham@users.noreply.github.com>
2021-08-28 22:14:30 +0530

    CaptionOnMemeTest - useEffect

--- react-js/src/DAY 22/Components/meme.jsx
+++ react-js/src/DAY 22/Components/meme.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Button, Input } from "@material-ui/core";
 
 const Meme = (props) => {
@@ -12,7 +12,13 @@ const Meme = (props) => {
     boxes: [],
   }); //image_caption is used to display the caption at the correct position (https://imgflip.com/api)
   const CaptionOnMemeTest = () => {
-    let url = `https://api.imgflip.com/caption_image?template_id=${caption_image.template_id}&username=${caption_image.username}&password=${caption_image.password}`;
+    let url = `https://api.imgflip.com/caption_image?template_id=${
+      caption_image.template_id
+    }&username=${caption_image.username}&password=${
+      caption_image.password
+    }&boxes[0][text]=${"%20"}`;
+
+    // console.log(url);
     //“?” in URL acts as separator, it indicates end of URL resource path and start of query parameters. When this form is used, the combined URI stands for the object which results from the query being applied to the original object.
     // var arr = [1, 2, 3, 4];
     // console.log(arr);
@@ -24,20 +30,26 @@ const Meme = (props) => {
     //   console.log(arr); to understand the map warning.
 
     caption_image.boxes.map((box, index) => {
-      return (url += `&boxes[${index}][text]=${box.text}`);
+      const text = box.text === "" ? "%20" : box.text;
+      return (url += `&boxes[${index}][text]=${text}`);
       // writing return doesnt make any sense here because we are not changing anything within caption_image.boxes
       // console.log(` boxes[${index}][text]=${box.text}`);
     });
-    // console.log(url);
+    console.log("2", url);
+
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
         const url2 = data && data.data && data.data.url; // Null checking, part of every version if data is false toh aagey dekhega hi nahi.
         // props.setMeme({ ...props.meme, url: data?.data?.url || "" }); //data?. null checking- ES7
-        props.setMeme({ ...props.meme, url: url2  });
+        props.setMeme({ ...props.meme, url: url2 });
       });
   };
+
+  useEffect(() => {
+    CaptionOnMemeTest();
+  }, [caption_image]);
 
   return (
     <div className="MemeBox">
@@ -56,7 +68,6 @@ const Meme = (props) => {
                 const data_boxes = caption_image.boxes;
                 data_boxes[index] = { text: e.target.value };
                 setCaption_image({ ...caption_image, boxes: data_boxes });
-                CaptionOnMemeTest();
               }}
             ></Input>
           ))}
